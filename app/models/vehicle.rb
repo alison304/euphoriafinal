63f176f65be4afeb58e4e93b457e91ddb6e178f7
Alison Paico <alison.jpq@gmail.com>
2022-01-27 20:04:21 -0500

    nuevos cambios

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,9 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+  validates :model, :color, presence: true
+  validates :license, uniqueness: true
+
 end
