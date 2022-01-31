1fce3ed8f88f1c1726c1c25abf1f88d4d9014601
Alison Paico <alison.jpq@gmail.com>
2022-01-30 19:51:10 -0500

    arreglo porque no habia imagen

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,6 +2,7 @@ class Vehicle < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+  validates :photo, presence: true
 
   validates :model, presence: true
   validates :license, uniqueness: true
